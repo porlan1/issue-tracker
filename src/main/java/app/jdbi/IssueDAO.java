b670ff723ca6da412b2838c193ab559ed70c290c
Paul Orlando <porlan3@gmail.com>
2018-09-18 23:25:00 -0400

    mostly working api, need to fix up GET from fetch call

--- src/main/java/app/jdbi/IssueDAO.java
+++ src/main/java/app/jdbi/IssueDAO.java
@@ -17,38 +17,33 @@ import java.util.Optional;
 public interface IssueDAO {
     
     //post insert: project, issue_title, issue_text, created_by, and optional assigned_to and status_text
-    /*@SqlUpdate("INSERT INTO issue (project, created_on, issue_title,
+    /*
+    INSERT INTO issue (project, created_on, issue_title,
     issue_text, created_by, assigned_to, status_text) values ( 
         :project,
-        CASE WHEN :created_on THEN :created_on ELSE created_on END,
-        CASE WHEN :issue_title THEN :issue_title ELSE issue_title END,
-        CASE WHEN :issue_text THEN :issue_text ELSE issue_text END,
-        CASE WHEN :created_by THEN :created_by ELSE created_by END,
-        CASE WHEN :assigned_to THEN :assigned_to ELSE assigned_to END,
-        CASE WHEN :status_text THEN :status_text ELSE status_text END)")
-    void insert(@Bind("project") String project, @Bind("created_on") String created_on, 
-    @Bind("issue_title") String issue_title, @Bind("issue_text") String issue_text, 
-    @Bind("created_by") String created_by, @Bind("assigned_to") String assigned_to, 
-    @Bind("status_text") String status_text);
+        now(),
+        :issue_title,
+        :issue_text,
+        :created_by,
+        :assigned_to,
+        :status_text)
     */
+    @SqlQuery("INSERT INTO issue (project, created_on, issue_title, issue_text, created_by, assigned_to, status_text) values (:project, now(), :issue_title, :issue_text, :created_by, :assigned_to, :status_text) RETURNING *")
+    Issue create(@Bind("project") String project,
+    @Bind("issue_title") Optional<String> issue_title, @Bind("issue_text") Optional<String> issue_text, 
+    @Bind("created_by") Optional<String> created_by, @Bind("assigned_to") Optional<String> assigned_to, 
+    @Bind("status_text") Optional<String> status_text);
 
     /*
     //put update: issue_title, issue_text, created_by, and optional assigned_to and status_text
     //with a _id and any fields in the object with a value to object said object. Returned will be 'successfully updated' or 'could not update '+_id
-    @SqlUpdate("UPDATE issue SET project = :project, 
-    created_on = CASE WHEN :created_on THEN :created_on ELSE created_on END, 
-    issue_title CASE WHEN :issue_title THEN :issue_title ELSE issue_title END,
-    issue_text = CASE WHEN :issue_text THEN :issue_text ELSE issue_text END,
-    created_by = CASE WHEN :created_by THEN :created_by ELSE created_by END,
-    assigned_to = CASE WHEN :assigned_to THEN :assigned_to ELSE assigned_to END,
-    status_text = CASE WHEN :status_text THEN :status_text ELSE status_text END
-    WHERE _id = :_id")
-    void update(@Bind("_id") int _id, @Bind("project") String project, @Bind("created_on") String created_on, 
-    @Bind("issue_title") String issue_title, @Bind("issue_text") String issue_text, 
-    @Bind("created_by") String created_by, @Bind("assigned_to") String assigned_to, 
-    @Bind("status_text") String status_text);
     */
-
+    @SqlUpdate("UPDATE issue SET project = :project, updated_on = now(), open = CASE WHEN :open IS NOT NULL THEN :open ELSE open END, issue_title = CASE WHEN :issue_title IS NOT NULL THEN :issue_title ELSE issue_title END, issue_text = CASE WHEN :issue_text IS NOT NULL THEN :issue_text ELSE issue_text END, created_by = CASE WHEN :created_by IS NOT NULL THEN :created_by ELSE created_by END, assigned_to = CASE WHEN :assigned_to IS NOT NULL THEN :assigned_to ELSE assigned_to END, status_text = CASE WHEN :status_text IS NOT NULL THEN :status_text ELSE status_text END WHERE _id = :_id")
+    Boolean update(@Bind("_id") Integer _id, @Bind("project") Optional<String> project, 
+    @Bind("open") Optional<Boolean> open,
+    @Bind("issue_title") Optional<String> issue_title, @Bind("issue_text") Optional<String> issue_text, 
+    @Bind("created_by") Optional<String> created_by, @Bind("assigned_to") Optional<String> assigned_to, 
+    @Bind("status_text") Optional<String> status_text);
     
     //I can GET /api/issues/{project} for an array of all issues on that specific project with all the information for each issue as was returned when posted.
     //I can filter my get request by also passing along any field and value in the query(ie. /api/issues/{project}?open=false). I can pass along as many fields/values as I want.
@@ -69,9 +64,8 @@ public interface IssueDAO {
     AND CASE WHEN :assigned_to THEN assigned_to = :assigned_to ELSE TRUE END
     AND CASE WHEN :status_text THEN status_text = :status-text ELSE TRUE END
     */
-    @SqlQuery("SELECT * FROM issue WHERE TRUE AND CASE WHEN :_id IS NOT NULL THEN _id = :_id ELSE TRUE END AND CASE WHEN :created_on::timestamp with time zone IS NOT NULL THEN created_on = :created_on::timestamp with time zone ELSE TRUE END AND CASE WHEN :updated_on::timestamp with time zone IS NOT NULL THEN updated_on = :updated_on::timestamp with time zone ELSE TRUE END AND CASE WHEN :issue_title IS NOT NULL THEN issue_title = :issue_title ELSE TRUE END AND CASE WHEN :issue_text IS NOT NULL THEN issue_text = :issue_text ELSE TRUE END AND CASE WHEN :created_by IS NOT NULL THEN created_by = :created_by ELSE TRUE END AND CASE WHEN :assigned_to IS NOT NULL THEN assigned_to = :assigned_to ELSE TRUE END AND CASE WHEN :status_text IS NOT NULL THEN status_text = :status_text ELSE TRUE END")
+    @SqlQuery("SELECT * FROM issue WHERE TRUE AND CASE WHEN :_id IS NOT NULL THEN _id = :_id ELSE TRUE END AND CASE WHEN :issue_title IS NOT NULL THEN issue_title = :issue_title ELSE TRUE END AND CASE WHEN :issue_text IS NOT NULL THEN issue_text = :issue_text ELSE TRUE END AND CASE WHEN :created_by IS NOT NULL THEN created_by = :created_by ELSE TRUE END AND CASE WHEN :assigned_to IS NOT NULL THEN assigned_to = :assigned_to ELSE TRUE END AND CASE WHEN :status_text IS NOT NULL THEN status_text = :status_text ELSE TRUE END")
     List<Issue> find(@Bind("_id") Optional <Integer> _id, @Bind("project") Optional <String> project, 
-    @Bind("created_on") Optional <Timestamp> created_on, @Bind("updated_on") Optional <Timestamp> updated_on, 
     @Bind("open") Optional <Boolean> open, @Bind("issue_title") Optional <String> issue_title,
     @Bind("issue_text") Optional <String> issue_text, @Bind("created_by") Optional<String> created_by,
     @Bind("assigned_to") Optional <String> assigned_to, @Bind("status_text") Optional <String> status_text);
@@ -83,6 +77,6 @@ public interface IssueDAO {
     /*
     //I can DELETE /api/issues/{project} with a _id to completely delete an issue. If no _id is sent return '_id error', success: 'deleted '+_id, failed: 'could not delete '+_id.
     */
-    @SqlQuery("DELETE FROM issue WHERE _id = :_id RETURNING *")
-    List<Issue> deleteById(@Bind("_id") int _id);
+    @SqlUpdate("DELETE FROM issue WHERE _id = :_id")
+    Boolean deleteById(@Bind("_id") int _id);
 }
--- src/main/java/app/resources/api/IssueTrackerResource.java
+++ src/main/java/app/resources/api/IssueTrackerResource.java
@@ -32,81 +32,71 @@ public class IssueTrackerResource {
         this.dao = dao;
     }
     
-    /*@POST
+    @POST
     @Timed
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    public Map<String, String> createIssue(@PathParam("project") String project,
-        @FormParam("accept") String accept) throws Exception {
-        final String value = String.format(Encode.forHtml(project));
-        IssueTracker issueTracker = new IssueTracker(value);
-        return issueTracker.convertInput();
-    }*/
+    public Issue createIssue(@PathParam("project") String project,
+    @QueryParam("issue_title") Optional <String> issue_title,
+    @QueryParam("issue_text") Optional<String> issue_text,
+    @QueryParam("created_by") Optional<String> created_by,
+    @QueryParam("assigned_to") Optional<String> assigned_to,
+    @QueryParam("status_text") Optional<String> status_text) throws Exception {
+        return dao.create(project,
+        issue_title, issue_text, 
+        created_by, assigned_to, 
+        status_text);
+    }
 
-    /*@PUT
+    @PUT
     @Timed
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    public Map<String, String> updateIssue(@QueryParam("_id") String _id,
-    @QueryParam("created_on") String created_on,
-    @QueryParam("updated_on") String updated_on,
-    @QueryParam("open") String open,
-    @QueryParam("issue_title") String issue_title,
-    @QueryParam("issue_text") String issue_text,
-    @QueryParam("created_by") String created_by,
-    @QueryParam("assigned_to") String assigned_to,
-    @QueryParam("status_text") String status_text) throws Exception {
-        final String value = String.format(Encode.forHtml(_id));
-        IssueTracker issueTracker = new IssueTracker(value);
-        return issueTracker.convertInput();
-    }*/
+    public String updateIssue(@QueryParam("_id") Integer _id,
+    @PathParam("project") Optional<String> project,
+    @QueryParam("open") Optional <Boolean> open,
+    @QueryParam("issue_title") Optional<String> issue_title,
+    @QueryParam("issue_text") Optional<String> issue_text,
+    @QueryParam("created_by") Optional<String> created_by,
+    @QueryParam("assigned_to") Optional <String> assigned_to,
+    @QueryParam("status_text") Optional <String> status_text) throws Exception {
+        if (_id == null || 
+        (project == null && open == null && 
+        issue_title == null && issue_text == null && 
+        created_by == null && assigned_to == null && 
+        status_text == null)) {
+            return "no updated field sent";
+        }
+        if (dao.update(_id, project, open,
+        issue_title, issue_text, 
+        created_by, assigned_to, 
+        status_text)) {
+            return "successfully updated";
+        }
+        return "could not update " + _id.toString();  
+    }
 
     @DELETE
     @Timed
-    public List <Issue> deleteIssue(@QueryParam("_id") int _id) throws Exception {
-        System.out.println(_id);
-        System.out.println(_id);
-        System.out.println(_id);
-        System.out.println(_id);
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        return dao.deleteById(_id);
+    public String deleteIssue(@QueryParam("_id") Integer _id) throws Exception {
+        if (_id == null) {
+            return "_id error";
+        }
+        if (dao.deleteById(_id)) {
+            return "deleted " + _id.toString();
+        }
+        return "could not delete " + _id.toString();
     }
-    
-    /*@GET
-    @Timed
-    public List<Issue> getIssues(
-    @QueryParam("_id") int _id) throws Exception {
-        //final String value = String.format(Encode.forHtml(project));
-        return dao.findById(_id);
-    }*/
 
     @GET
     @Timed
     public List<Issue> getIssues(@PathParam("project") Optional <String> project,
     @QueryParam("_id") Optional <Integer> _id,
-    @QueryParam("created_on") Optional <Timestamp> created_on,
-    @QueryParam("updated_on") Optional <Timestamp> updated_on,
     @QueryParam("open") Optional <Boolean> open,
     @QueryParam("issue_title") Optional <String> issue_title,
     @QueryParam("issue_text") Optional <String> issue_text,
     @QueryParam("created_by") Optional <String> created_by,
     @QueryParam("assigned_to") Optional <String> assigned_to,
     @QueryParam("status_text") Optional <String> status_text) throws Exception {
-        //final String value = String.format(Encode.forHtml(project));
-        return dao.find(_id, project, created_on, updated_on, open, issue_title, issue_text, created_by, assigned_to, status_text);
+        return dao.find(_id, project, open, issue_title, issue_text, created_by, assigned_to, status_text);
     }
 
-    /*@GET
-    @Timed
-    public Response converter(@QueryParam("input") String input) throws Exception {
-        try {
-            final String value = String.format(Encode.forHtml(input));
-            Converter converter = new Converter(value);
-            return Response.ok(converter.convertInput()).type(MediaType.APPLICATION_JSON).build();
-        } catch (Exception e) {
-            return Response.ok(e.getMessage()).type(MediaType.TEXT_PLAIN).build();
-        }
-    }*/
 }
